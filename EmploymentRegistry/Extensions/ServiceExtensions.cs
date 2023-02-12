c883e224e92fe53450f47d22d6b90c241961814f
Paul <pavel.k.313@mail.ru>
2023-02-12 19:12:14 +0300

    created extension methods to configure services in Program

---
+++ EmploymentRegistry/Extensions/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace EmploymentRegistry.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection serviceDescriptors) =>
+            serviceDescriptors.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection serviceDescriptors) =>
+            serviceDescriptors.Configure<IISOptions>(options =>
+            {
+
+            });
+    }
+}
--- EmploymentRegistry/Program.cs
+++ EmploymentRegistry/Program.cs
@@ -1,4 +1,11 @@
+using EmploymentRegistry.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add Service Configuration by Extension Methods.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 // Add services to the container.
 
@@ -7,8 +14,20 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else 
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
