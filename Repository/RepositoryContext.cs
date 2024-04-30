9ed28674627c26692b15564ccfe02ee220b8a1e2
Paul <pavel.k.313@mail.ru>
2024-04-30 16:46:18 +0300

    added Repository configuration classes for DB seeding with initial data(usage in OnModelCreating())

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Entities.Entities;
+using Repository.Configuration;
 
 namespace Repository
 {
     public class RepositoryContext : DbContext
     {
         public RepositoryContext(DbContextOptions options) : base(options) { }
+
+        // seeding DB with initial data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
 
         public DbSet<Company>? Companies { get; set; }
         public DbSet<Employee>? Employees { get; set; }
