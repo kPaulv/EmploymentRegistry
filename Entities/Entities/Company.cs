983fd078682480db53fbf39b60ac1bc5d58a0d9b
Paul <pavel.k.313@mail.ru>
2024-04-30 15:55:54 +0300

    created Repository project for DbContext and Entities project for DB tables. added references, added Context Factory for creating DbContext instances in the startup project

---
+++ Entities/Entities/Company.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    public class Company
+    {
+        [Column("CompanyId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(60, ErrorMessage = "Max length for Company name is 60 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Company address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Max length for Compay address is 60 characters")]
+        public string? Address { get; set; }
+
+        public string? Country { get; set; }
+
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities.Entities;
+
+namespace Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
