7d78a93c2a1599162fa0d304ee8d8552464c6b38
Paul <pavel.k.313@mail.ru>
2024-07-15 17:34:12 +0300

    RootController added for listing APIs

---
+++ Presentation/Controllers/RootController.cs
@@ -0,0 +1,51 @@
+﻿using Entities.LinkModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public RootController(LinkGenerator linkGenerator) => _linkGenerator = linkGenerator;
+
+        [HttpGet(Name = "GetRoot")]
+        public IActionResult GetRoot([FromHeader(Name = "Accept")] string headerMediaType)
+        {
+            if (headerMediaType.Contains("application/vnd.kpaulv.apiroot"))
+            {
+                var apiLinksList = new List<Link>() 
+                {
+                    new Link()
+                    {
+                        Href = _linkGenerator.GetUriByName(HttpContext, nameof(GetRoot), new { }),
+                        Rel = "self",
+                        Method = "GET"
+                    },
+                    new Link()
+                    {
+                        Href = _linkGenerator.GetUriByName(HttpContext, "CompanyById", new { }),
+                        Rel = "company",
+                        Method = "GET"
+                    },
+                    new Link()
+                    {
+                        Href = _linkGenerator.GetUriByName(HttpContext, "CompanyCollection", new { }),
+                        Rel = "companies",
+                        Method = "GET"
+                    }
+                };
+                return Ok(apiLinksList);
+            }
+            return NoContent();
+        }
+    }
+}
